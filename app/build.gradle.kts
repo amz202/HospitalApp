1ceba76114952e5f22dafdfa41fc7b309db7d3d1
Abdul Majid <abdulmajidzeeshan4@gmail.com>
2025-05-09 02:54:34 +0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
     // Architecture Components
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
---
+++ app/src/main/java/com/example/hospitalapp/network/data/AppContainer.kt
@@ -0,0 +1,79 @@
+package com.example.hospitalapp.network.data
+
+import com.example.hospitalapp.network.ApiService
+import com.example.hospitalapp.network.data.repositories.AdminRepository
+import com.example.hospitalapp.network.data.repositories.AdminRepositoryImpl
+import com.example.hospitalapp.network.data.repositories.AppointmentRepository
+import com.example.hospitalapp.network.data.repositories.AppointmentRepositoryImpl
+import com.example.hospitalapp.network.data.repositories.DoctorRepository
+import com.example.hospitalapp.network.data.repositories.DoctorRepositoryImpl
+import com.example.hospitalapp.network.data.repositories.FeedbackRepository
+import com.example.hospitalapp.network.data.repositories.FeedbackRepositoryImpl
+import com.example.hospitalapp.network.data.repositories.MedicationRepository
+import com.example.hospitalapp.network.data.repositories.MedicationRepositoryImpl
+import com.example.hospitalapp.network.data.repositories.PatientRepository
+import com.example.hospitalapp.network.data.repositories.PatientRepositoryImpl
+import com.example.hospitalapp.network.data.repositories.ReportRepository
+import com.example.hospitalapp.network.data.repositories.ReportRepositoryImpl
+import com.example.hospitalapp.network.data.repositories.VitalsRepository
+import com.example.hospitalapp.network.data.repositories.VitalsRepositoryImpl
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val patientRepository: PatientRepository
+    val adminRepository: AdminRepository
+    val feedbackRepository: FeedbackRepository
+    val appointmentRepository: AppointmentRepository
+    val doctorRepository: DoctorRepository
+    val medicationRepository : MedicationRepository
+    val vitalsRepository: VitalsRepository
+    val reportRepository: ReportRepository
+}
+
+class DefaultAppContainer:AppContainer {
+    val BASE_URL = "http://192.168.208.28:5432/"
+    val json = Json {
+        this.ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .build()
+
+    val apiService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+    override val reportRepository: ReportRepository by lazy {
+        ReportRepositoryImpl(apiService)
+    }
+    override val patientRepository: PatientRepository by lazy {
+        PatientRepositoryImpl(apiService)
+    }
+    override val adminRepository: AdminRepository by lazy {
+        AdminRepositoryImpl(apiService)
+    }
+
+    override val feedbackRepository: FeedbackRepository by lazy {
+        FeedbackRepositoryImpl(apiService)
+    }
+
+    override val appointmentRepository: AppointmentRepository by lazy {
+        AppointmentRepositoryImpl(apiService)
+    }
+
+    override val doctorRepository: DoctorRepository by lazy {
+        DoctorRepositoryImpl(apiService)
+    }
+
+    override val medicationRepository: MedicationRepository by lazy {
+        MedicationRepositoryImpl(apiService)
+    }
+
+    override val vitalsRepository: VitalsRepository by lazy {
+        VitalsRepositoryImpl(apiService)
+    }
+}
